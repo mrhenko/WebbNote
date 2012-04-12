ed3759d2446a65dee496263a51770ce3f6b3ecd9
Mikael Jorhult <mikael@jorhult.se>
2012-04-12 13:21:56 +0200

    Optimized selectors

--- webbnote.js
+++ webbnote.js
@@ -2,31 +2,33 @@
 	$(document).ready(function() { 
 		// Skapa en lista över de olika sidorna (.page)
 		// och lägg dem i en snabbnavigeringsmeny
-		var quicknav = $('ul#quicknav');
+		var quicknav = $('#quicknav');
 		$('#keynote .page').each(function () {
-			var data_visible = $(this).attr('data-visible');
-			var li_name = $(this).find('h2').html();
+			var $this = $(this),
+				data_visible = $this.attr('data-visible'),
+				li_name = $this.find('h2').html();
+				
 			if (li_name == null) {
-				li_name = $(this).find('h1').html();
+				li_name = $this.find('h1').html();
 			}
+			
 			var new_li = '<li class="page_quicknav"><a href="#" data-time="' + data_visible + '">' + li_name + '</a></li>';
 			$(quicknav).append(new_li);
 		});
 	
 		// Skapa variabler för keynoten och ljudet för att
 		// snabba upp det hela.
-		
 		var keynote = $('#keynote');
 		var audio = $('#presentation').get(0);
 		
 		// Gör progressbaren lika "lång" som låten
-		var progressbar = $('#player #progressbar');
+		var progressbar = $('#progressbar');
 		var duration = Math.round(audio.duration);
 		
 		$(progressbar).attr('max', duration);
 		
 		// Mediaspelarkontroller
-		$('#player #playpause').click(function() {
+		$('#playpause').click(function() {
 			if (audio.paused) {
 				audio.play();
 			} else {
@@ -34,13 +36,15 @@
 			} 
 		});
 		
+		var $player = $('#player');
+		
 		// Stoppknappen
-		$('#player .stop').click(function() {
+		$player.find('.stop').click(function() {
 			audio.pause();
 			audio.currentTime = 0;
 		});
 		
-		$('#player .repeat').click(function() {
+		$player.find('.repeat').click(function() {
 			var newtime = audio.currentTime;
 			newtime = newtime - 10;
 			console.log(newtime);
@@ -50,12 +54,12 @@
 		});
 		
 		$(audio).bind('play', function() {
-			$('#player #playpause').addClass('pause');
-			$('#player #playpause').removeClass('play');
+			$('#playpause').addClass('pause');
+			$('#playpause').removeClass('play');
 		});
 		$(audio).bind('pause', function() {
-			$('#player #playpause').addClass('play');
-			$('#player #playpause').removeClass('pause');
+			$('#playpause').addClass('play');
+			$('#playpause').removeClass('pause');
 		});
 		
 		// En event-hanterare för när ljudet spelas
@@ -127,21 +131,23 @@ function init_webbnote(files) {
 			till ms.
 		*/
 		$('[data-visible]').each(function() {
-			var visitime = $(this).attr('data-visible');
+			var $this = $(this),
+				visitime = $this.attr('data-visible');
+				
 			if (visitime.search(':') != -1) {
 				// Det här är en data-variabel som formaterats enligt hh:mm:ss
 				// Konvertera den.
 				var time_array = visitime.split(':');
 				new_time = hms_to_ms(time_array);	
-				$(this).attr('data-visible', new_time);
+				$this.attr('data-visible', new_time);
 			}
 			
-			var hidetime = $(this).attr('data-hide');
+			var hidetime = $this.attr('data-hide');
 			if (hidetime) {			
 				if (hidetime.search(':') != -1) {
 					time_array = hidetime.split(':');
 					new_time = hms_to_ms(time_array);
-					$(this).attr('data-hide', new_time);
+					$this.attr('data-hide', new_time);
 				}
 			}
 			
